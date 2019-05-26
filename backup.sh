ee692c09594836d4b66aca04a6686becb92ff4d6
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2019-05-26 17:50:55 +0200

    Added more to backup.

--- backup.sh
+++ backup.sh
@@ -12,10 +12,10 @@
 
 #set -x
 
-SVN="Lectures Makefiles Research bin devel pub"
+SVN="Lectures Makefiles Research bin pub"
 LINK="tools Official"
 #MAIN="${SVN} ${LINK} Config WebPub bib man misc Library/Thunderbird/Profiles/56rgu1dz.default"
-MAIN="${SVN} ${LINK} Desktop SVN.dump bib .thunderbird git/ASN.1 git/Book git/Caml_light-build git/Csharp_lexer git/Csharp_preprocessor git/CV git/LaTeX-build git/Mini-ML git/Music git/OCaml-build git/PhD git/Poetry git/Red-Black_Trees git/Scripts git/Singapore git/Ternary_Search_Trees git/UnionFind"
+MAIN="${SVN} ${LINK} Desktop SVN.dump bib .thunderbird git/ASN.1 git/Book git/Caml_light-build git/Csharp_lexer git/Csharp_preprocessor git/CV git/LaTeX-build git/Mini-ML git/Music git/OCaml-build git/PhD git/Poetry git/Red-Black_Trees git/Scripts git/Singapore git/Ternary_Search_Trees git/UnionFind academia.html"
 CONF=".bashrc .bash_profile .bash_logout .emacs .profile .my_dircolors .xmodmap_apple .emacs_modes .ssh"
 #ALL="SVN ${MAIN} ${CONF}"
 ALL="${MAIN} ${CONF}"
@@ -25,7 +25,7 @@ prefix=/media/rinderkn/Backup
 base=snapshot
 new_snapshot="${prefix}/${base}.00"
 #scripts=/Volumes/Users/rinderkn/devel/Scripts
-scripts=/home/rinderkn/devel/Scripts
+scripts=/home/rinderkn/git/Scripts
 
 (cd ~
 #$scripts/clean.sh --recursive ${SVN} ${LINK} public_html/CV public_html/Software
@@ -35,10 +35,10 @@ printf "synchronising $new_snapshot with public_html..."
 # Removed --perms --group --owner for exFAT
 rsync --recursive --copy-links --times \
 --exclude=.svn --ignore-errors \
---delete --delete-excluded --exclude=Lectures/ --exclude=Mirror/ \
+--delete --delete-excluded --exclude=Lectures/ \
 public_html $new_snapshot
 echo " done."
-for path in bin devel tools; do
+for path in bin tools; do
   $scripts/symlinks.sh --set --recursive $path
 done
 $scripts/symlinks.sh --set public_html
