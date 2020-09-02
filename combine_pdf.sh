5bf1d3a7d4f556d0330afc3283a877f808c05d58
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2020-09-02 11:53:00 +0200

    Update.

--- combine_pdf.sh
+++ combine_pdf.sh
@@ -2,6 +2,12 @@
 
 # set -x
 
+if [ $# -lt 2 ]
+then
+    echo "Usage: combine_pdf.sh output.pdf input1.pdf input2.pdf ..."
+    exit 1
+fi
+
 target="$1"
 shift
 gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=$target -dBATCH "$@"
